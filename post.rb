12e9939c86d3305b43633b61c4ce9f84a04a2610
fjur <fjuristovski@gmail.com>
2016-03-09 19:28:24 +0000

    first commit

---
+++ post.rb
@@ -0,0 +1,22 @@
+class Post
+
+  def initialize
+    @title = #hacker news title
+    @url = #hacker news url
+    @points = #number points post has
+    @item_id = #item id of post on HN
+  end
+
+  def post
+    #returns all the comments on a particular post
+  end
+
+  def comment(comment_obj)
+    #add comment to the list
+  end
+
+end
+
+
+
+# puts doc
